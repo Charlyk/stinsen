bdc5cfc3477d5da3445a597bf65d5e141cb739ab
Eduard Albu <eduard.albu@voya.ai>
2023-02-10 11:52:09 +0200

    Using stinsen fork

--- Sources/Stinsen/NavigationCoordinatable/NavigationRouter.swift
+++ Sources/Stinsen/NavigationCoordinatable/NavigationRouter.swift
@@ -4,14 +4,14 @@ import SwiftUI
 public final class NavigationRouter<T: NavigationCoordinatable>: Routable {
     public let id: Int
     public var coordinator: T {
-        _coordinator.value!
+        _coordinator
     }
     
-    private var _coordinator: WeakRef<T>
+    private var _coordinator: T
     
     public init(id: Int, coordinator: T) {
         self.id = id
-        self._coordinator = WeakRef(value: coordinator)
+        self._coordinator = coordinator
     }
 }
 
